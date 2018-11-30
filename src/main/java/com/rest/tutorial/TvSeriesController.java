b5040e090930d662969243fd23cbfa90bad2150f
rippleberg <jimmyxbw@gmail.com>
2018-11-29 22:40:09 -0500

    a

--- src/main/java/com/rest/tutorial/TvSeriesController.java
+++ src/main/java/com/rest/tutorial/TvSeriesController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tvseries")
 public class TvSeriesController {
+	private static final Log log = LogFactory.getLog(TvSeriesController.class);
 
 	@GetMapping
 	public List<TvSeriesDto> getAll() {
+		if (log.isTraceEnabled()) {
+			log.trace("getAll() is called");
+		}
 		List<TvSeriesDto> list = new ArrayList<>();
 		Calendar calendar = Calendar.getInstance();
-		calendar.set(2016, 10, 2, 0, 0);
+		calendar.set(2016, Calendar.OCTOBER, 2, 0, 0);
 		list.add(new TvSeriesDto(1, "WestWorld", 1, calendar.getTime()));
+		calendar.set(2011, Calendar.SEPTEMBER, 22, 0, 0);
+		list.add(new TvSeriesDto(2, "Person of Interest", 5, calendar.getTime()));
 		return list;
 	}
 }
