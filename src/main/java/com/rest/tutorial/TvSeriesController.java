1db0454c1a9982abebf08fc57119f52307244320
rippleberg <jimmyxbw@gmail.com>
2018-12-01 09:18:06 -0500

    a

--- src/main/java/com/rest/tutorial/TvSeriesController.java
+++ src/main/java/com/rest/tutorial/TvSeriesController.java
@@ -1,19 +1,31 @@
 package com.rest.tutorial;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.commons.io.IOUtils;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/tvseries")
@@ -65,6 +77,44 @@ public class TvSeriesController {
 		}
 		throw new ResourceNotFoundException();
 	}
+	
+	@DeleteMapping("/{id}")
+	public Map<String, String> deleteOne(@PathVariable int id, HttpServletRequest request,
+			@RequestParam(value = "delete_reason", required = false) String deleteReason) {
+		if (log.isTraceEnabled()) {
+			log.trace("deleteOne " + id);
+		}
+		Map<String, String> res = new HashMap<>();
+		if (id == 101) {
+			res.put("message", "#101 is deleted by " + request.getRemoteAddr() + 
+					" and(reason: " + deleteReason + ")");
+		} else if (id == 102) {
+			throw new RuntimeException("#120 cannot be deleted");
+		} else {
+			throw new ResourceNotFoundException();
+		}
+		return res;
+	}
+	
+	@PostMapping(value="/{id}/photos", consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
+	public void addPhoto(@PathVariable int id, @RequestParam("photo") MultipartFile imgFile) throws Exception {
+		if (log.isTraceEnabled()) {
+			log.trace(id + " has received the file with file: " + imgFile.getOriginalFilename());
+		}
+		FileOutputStream fos = new FileOutputStream("target/" + imgFile.getOriginalFilename());
+		IOUtils.copy(imgFile.getInputStream(), fos);
+		fos.close();
+	}
+	
+    @GetMapping(value="/{id}/icon", produces=MediaType.IMAGE_JPEG_VALUE)
+    public byte[] getIcon(@PathVariable int id) throws Exception{
+        if(log.isTraceEnabled()) {
+            log.trace("getIcon(" + id + ")");
+        }
+        String iconFilePath = "src/test/resources/icon.jpg";
+        InputStream is = new FileInputStream(iconFilePath);
+        return IOUtils.toByteArray(is);
+    }
 	
     /**
      * 创建电视剧“Person of Interest",仅仅方便此节做展示其他方法用，以后章节把数据存储到数据库后，会删除此方法
